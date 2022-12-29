f63e4a2f534f644784060d8db49f3c26d05fa06e
sinan <aslansinan586@gmail.com>
2022-12-29 23:56:11 +0300

    useEffect kullanımı

--- src/App.js
+++ src/App.js
@@ -1,31 +1,55 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 function App() {
-  const [name, setName] = useState("Tuğba");
-  const [age, setAge] = useState(26);
-  const [friends, setFriends] = useState(["sinan","tuğba"]);
-  const [address, setAddress] = useState({title:"İstanbul",zip:34567});
+  const [number,setNumber] = useState(1);
+  const [name,setName] = useState('sinan');
+  useEffect(() => {
+    console.log("number state güncellendi.");
+  },[number]);
+
+  useEffect(() => {
+    console.log("name state güncellendi.");
+  },[name]);
+
+  useEffect(() => {
+    console.log('componeent mount edildi.')
+  },[]);
   return (
     <div className="App">
-      <h1>Merhaba {name}</h1>
-      <h2>{age}</h2> 
-      <button onClick={() => setName("sinan")}>change name</button>
-      <button onClick={() => setAge(25)}>change age</button>
-      <hr />
-      <h2>Friends</h2>
-      {friends.map((friend,key) => (
-        <div key={key}>{friend}</div>
-      ))
-      }
-      <button onClick={() => setFriends([...friends,"ayşe"])}>add new friend</button>
+     <h1>{number}</h1>
+     <button onClick={() => setNumber(number +1 )}>Click</button>
 
-      <hr />
-      <h2>Address</h2>
-      <div>
-        {address.title},{address.zip}
-      </div>
-      <button onClick={() => setAddress({...address,title:"Karabük",zip:232})}>add new address</button>
+     <hr />
+     <h1>{name}</h1>
+     <button onClick={() => setName("Tuğba")}>Click</button>
+
     </div>
   );
 }
 
 export default App;
+
+// using states 
+// const [name, setName] = useState("Tuğba");
+// const [age, setAge] = useState(26);
+// const [friends, setFriends] = useState(["sinan","tuğba"]);
+// const [address, setAddress] = useState({title:"İstanbul",zip:34567});
+
+
+// <h1>Merhaba {name}</h1>
+// <h2>{age}</h2> 
+// <button onClick={() => setName("sinan")}>change name</button>
+// <button onClick={() => setAge(25)}>change age</button>
+// <hr />
+// <h2>Friends</h2>
+// {friends.map((friend,key) => (
+//   <div key={key}>{friend}</div>
+// ))
+// }
+// <button onClick={() => setFriends([...friends,"ayşe"])}>add new friend</button>
+
+// <hr />
+// <h2>Address</h2>
+// <div>
+//   {address.title},{address.zip}
+// </div>
+// <button onClick={() => setAddress({...address,title:"Karabük",zip:232})}>add new address</button>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import Counter from './components/Counter';
 import InputExample from './components/InputExample';
 import reportWebVitals from './reportWebVitals';
@@ -10,8 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Counter /> */}
-    {/* <App /> */}
-    <InputExample />
+    <App /> 
+    {/* <InputExample /> */}
   </React.StrictMode>
 );
 
